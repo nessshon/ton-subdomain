b012fb4f72fb09b978e7634e6f452a6888ebc95d
nessshon <shonness98@gmail.com>
2024-01-10 00:22:21 +0500

    Add support for .t.me domains.

--- bot/app/bot/handlers/inline_query.py
+++ bot/app/bot/handlers/inline_query.py
@@ -5,7 +5,7 @@ from pytonapi.schema.nft import NftItem
 
 from app.bot.manager import Manager
 from app.bot.utils.states import State
-from app.config import get_dns_collection_address
+from app.config import get_dns_collections
 
 router = Router()
 router.inline_query.filter(F.chat_type == "sender")
@@ -14,19 +14,23 @@ router.inline_query.filter(F.chat_type == "sender")
 @router.inline_query(State.main_menu)
 async def select_deposit_nft(inline_query: InlineQuery, manager: Manager, atc_manager: ATCManager) -> None:
     offset = int(inline_query.offset) if inline_query.offset else 0
+    collections, items = get_dns_collections(manager.is_testnet), []
 
-    items = await manager.tonapi.accounts.get_nfts(
-        atc_manager.user.account_wallet.address,
-        collection=get_dns_collection_address(manager.is_testnet),
-        limit=50, offset=offset, indirect_ownership=False,
-    )
+    for collection_address in collections:
+        response = await manager.tonapi.accounts.get_nfts(
+            atc_manager.user.account_wallet.address,
+            collection=collection_address,
+            limit=25, offset=offset, indirect_ownership=False,
+        )
+        items += response.nft_items
+
     results = [
         create_nft_article(item) for item in
-        sorted(items.nft_items, key=lambda item: item.index)
+        sorted(items, key=lambda item: item.index)
     ]
 
     if results:
-        next_offset = str(offset + 50)
+        next_offset = str(offset + len(items))
         await inline_query.answer(
             results=results, cache_time=10, is_personal=True, next_offset=next_offset
         )
